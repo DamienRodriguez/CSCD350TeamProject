6eb964b4191958d8ba2cf9c8e4418cc969246c93
DamienRodriguez <47016137+DamienRodriguez@users.noreply.github.com>
2020-05-09 20:29:08 -0700

    Clean up of some methods.

--- SQLite_sandbox/src/DamiensDBConnector.java
+++ SQLite_sandbox/src/DamiensDBConnector.java
@@ -1,4 +1,3 @@
-import java.rmi.server.ExportException;
 import java.sql.*;
 
 public class DamiensDBConnector {
@@ -24,21 +23,38 @@ public class DamiensDBConnector {
     }
 
 
-    public void trueFalseAddQuestion(final int difficulty, final String question, final String answer, final String hint) throws Exception {
+    public void trueFalseAddQuestion(final int difficulty, final String question, final String answer, final String hint) throws Exception{
         String sql = "insert into trueFalse values(" + difficulty + ", '" + question + "', '" + answer + "', '" + hint + "')";
         executeStatement(sql);
     } //this will be the same but for the other queries for adding stuff into the database, so this will have to be refactored
       // for a more elegant solution. This smells like a template or builder type of problem (at least I think...)
+
 
     public void mutipleChoiceAddQuestion(final int difficulty, final String question, final String answer, final String wrongAnswer1, final String wrongAnswer2, final String wrongAnswer3, final String hint) throws Exception {
         String sql = "insert into multipleChoice values(" + difficulty + ", '" + question + "', '" + answer + "', '" + wrongAnswer1 + "', '" + wrongAnswer2 + "', '" + wrongAnswer3 + "', '" + hint + "')";
         executeStatement(sql);
     }
 
-    private void executeStatement(final String query) throws Exception {
-        Statement myStm = this.c.createStatement();
-        myStm.executeUpdate(query);
+
+    public void shortAnswerQuestions(final int difficulty, final String question, final String answer, final String hint) throws Exception {
+        String sql = "insert into shortAnswer values(" + difficulty + ", '" + question + "', '" + answer + "', '" + hint + "')";
+        executeStatement(sql);
     }
+
+
+    private void executeStatement(final String query) throws Exception {
+        try {
+            Statement myStm = this.c.createStatement();
+            myStm.executeUpdate(query);
+            System.out.println("Insertion complete");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+
 
 
 }
--- SQLite_sandbox/src/Tester.java
+++ SQLite_sandbox/src/Tester.java
@@ -4,9 +4,5 @@ public class Tester {
 
     public static void main(String[]args) throws Exception {
         DamiensDBConnector connection = new DamiensDBConnector();
-
-        //connection.trueFalseAddQuestion(1, "The Happy part of Stu Land is the best part.", "T", "Bad part is bad.");
-        connection.mutipleChoiceAddQuestion(1, "How many years are there between world fairs?", "5 years", "7 years", "10 years", "4 years", "it is 5 years.");
-
     }
 }
