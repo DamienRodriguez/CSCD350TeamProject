1a989248c03caad9ca241718397ad23eba1f52e2
Damien Rodriguez <drodriguez17@eagles.ewu.edu>
2020-05-15 03:17:41 -0700

    Basic unit tests created.

---
+++ TriviaDataBase/tests/QuestionTest.java
@@ -0,0 +1,59 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class QuestionTest {
+
+
+    @Test
+    void getID() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getDiff() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getQuestion() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getAnswer() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getHint() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getWrongAnswerOne() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getWrongAnswerTwo() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getWrongAnswerThree() {
+        assertEquals(0, 1);
+    }
+
+
+    @Test
+    void getIDParseArray() {
+        assertEquals(0, 1);
+    }
+}
