9616780fcd5c1f89f443f8399d689d9e3103da7d
PhilipSiler <63834552+PhilipSiler@users.noreply.github.com>
2020-06-01 12:43:17 -0700

    Add files via upload

--- QuestionReader.java
+++ QuestionReader.java
@@ -10,36 +10,71 @@ class QuestionReader {
     File file = new File("Questions.txt");
     Scanner sc = new Scanner(file);
     String[] question = null;
+    int counter = 1;
     while (sc.hasNextLine()){
         question = sc.nextLine().split(" ! ");
         if (question.length == 6)
-            addQuestionMC(question);
+            addQuestionMC(question, counter);
         else if (question.length == 3){
             if (question[1].equalsIgnoreCase("t") || question[1].equalsIgnoreCase("f"))
-                addQuestionTF(question);
+                addQuestionTF(question, counter);
             else
-                addQuestionSA(question);
+                addQuestionSA(question, counter);
         }
+        counter ++;
     }//end while loop
     sc.close();
 }
 
-	private static void addQuestionSA(String[] question) {
+	private static void addQuestionSA(String[] question, int counter) {
         //System.out.println("Adding Question SA.");
+        String qid = "";
+        if (counter <= 32){
+            qid = "ees";
+        }
+        if (counter > 32 && counter <= 64){
+            qid = "mes";
+        }
+        if (counter > 64 && counter <= 96){
+            qid = "hes";
+        }
+        System.out.println(qid);
         for (int x = 0; x < question.length; x ++){
             System.out.println(question[x]);
         }
     }
 
-    private static void addQuestionTF(String[] question) {
+    private static void addQuestionTF(String[] question, int counter) {
         //System.out.println("Adding Question TF.");
+        String qid = "";
+        if (counter <= 32){
+            qid = "eet";
+        }
+        if (counter > 32 && counter <= 64){
+            qid = "met";
+        }
+        if (counter > 64 && counter <= 96){
+            qid = "het";
+        }
+        System.out.println(qid);
         for (int x = 0; x < question.length; x ++){
             System.out.println(question[x]);
         }
     }
 
-    private static void addQuestionMC(String[] question) {
+    private static void addQuestionMC(String[] question, int counter) {
         //System.out.println("Adding Question MC.");
+        String qid = "";
+        if (counter <= 32){
+            qid = "eem";
+        }
+        if (counter > 32 && counter <= 64){
+            qid = "mem";
+        }
+        if (counter > 64 && counter <= 96){
+            qid = "hem";
+        }
+        System.out.println(qid);
         for (int x = 0; x < question.length; x ++){
             System.out.println(question[x]);
         }
