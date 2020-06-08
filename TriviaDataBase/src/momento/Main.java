5f7954b61f3143a4dd88975cb56c0fa181a00599
Damien Rodriguez <drodriguez17@eagles.ewu.edu>
2020-06-08 15:21:26 -0700

    Initial commit
    Added momento package for saving game
    
    Co-Authored-By: Kevin Underwood <kunderwood1@eagles.ewu.edu>

---
+++ TriviaDataBase/src/momento/Main.java
@@ -0,0 +1,40 @@
+package momento;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing memento saveState.");
+        Caretaker caretaker = new Caretaker(); //YOU MUST ADD THIS TO THE MAZE DRIVER
+        Originator originator = new Originator(); //YOU MUST ADD THIS TO THE MAZE DRIVER
+        Scanner kb = new Scanner(System.in);
+        saveGame(caretaker, originator, kb);
+        Memento mySave = loadGame(caretaker, originator, kb);
+
+        //System.out.println(mySave.toString());
+        kb.close();
+    }
+
+    public static void saveGame(Caretaker caretaker, Originator originator, Scanner kb){
+        System.out.println("What would like to call your save game?");
+        String saveName = kb.nextLine();
+
+        //SAVEGAME MUST BE ADDED TO MAZE() C
+        int cursor = 10; 
+        int[] playerPosition = new int[] {3,3};
+        Room[][] maze = new Room[5][5];
+        
+        originator.setState(saveName, cursor, playerPosition, maze);
+        caretaker.addMemento(originator.save());
+    }
+
+    public static Memento loadGame(Caretaker caretaker, Originator originator, Scanner kb){
+        String saveName = "";
+        Memento mySave = null;
+        while (saveName == null || saveName == ""){
+            System.out.println("Please enter the save name: ");
+            saveName = kb.nextLine();
+            mySave = caretaker.getMemento(saveName);
+        }
+        return mySave;
+    }
+}
---
+++ TriviaDataBase/src/momento/Memento.java
@@ -0,0 +1,53 @@
+package momento;
+class Memento{
+    private Memento state;
+    private String saveName;
+    private int cursor;
+    private int[] playerPosition;
+    private Room[][] maze;
+ 
+    public Memento(String saveName, int cursor, int[] playerPosition, Room[][] maze)
+    {
+        this.saveName = saveName;
+        this.cursor = cursor;
+        this.playerPosition = playerPosition;
+        this.maze = maze;
+    }
+
+    public Memento getState()
+    {
+        return state;
+    }
+
+    public String toString() //for testing and debug purposes, the user will not see this code.
+    {
+        String str = "Save Name: " + saveName;
+        
+        str += ", the cursor is at: " + cursor;
+
+        str += ", Player Position: X, Y";
+        for (int fromPos : playerPosition)
+        {
+            str += ", " + fromPos;
+        }
+
+        return str;
+    }
+
+    public String getName()
+    {
+        return saveName;
+    }
+
+    public int getCursor(){
+        return cursor;
+    }
+
+    public int[] getPosition(){
+        return playerPosition;
+    }
+
+    public Room[][] getMaze(){
+        return maze;
+    }
+}
